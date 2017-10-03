da1b5594f4d7cc9d0e1489a04636a68597cde002
Greg <neonmd@hotmail.co.uk>
2017-10-03 16:02:10 +0100

    rails g react:component post title body --es6

---
+++ app/assets/javascripts/components/post.es6.jsx
@@ -0,0 +1,16 @@
+class Post extends React.Component {
+  render () {
+    return (
+      <div>
+        <div>Title: {this.props.title}</div>
+        <div>Body: {this.props.body}</div>
+      </div>
+    );
+  }
+}
+
+Post.propTypes = {
+  title: React.PropTypes.node,
+  body: React.PropTypes.node
+};
+
